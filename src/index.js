e141cccc89917747089d2829579333be08b76bb6
nedned1108@github.com <rpmshockwave@gmail.com>
2023-09-26 11:15:57 -0400

    added store index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
