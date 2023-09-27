18c34612a074b54bc3999c44a600515ba7b02bf8
nedned1108@github.com <rpmshockwave@gmail.com>
2023-09-27 16:09:02 -0400

    updated cart page

--- src/components/CartPage/index.js
+++ src/components/CartPage/index.js
@@ -1,11 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadCartThunk, addToCartThunk, removeFromCartThunk, updateCartThunk } from "../../store/cart";
 
 function CartPage() {
   const dispatch = useDispatch();
   const cart = useSelector(state => state.cart.products);
-  console.log(cart);
 
   useEffect(() => {
     dispatch(loadCartThunk());
@@ -18,7 +17,7 @@ function CartPage() {
   const handleRemoveFromCart = (productId) => {
     dispatch(removeFromCartThunk(productId));
   }
-  
+
   const handleUpdateCart = (product) => {
     dispatch(updateCartThunk(product));
   }
@@ -29,32 +28,49 @@ function CartPage() {
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-3xl font-bold">Cart</h1>
+      <h1 className="text-3xl font-bold">Shopping Cart</h1>
       <div className="flex flex-col items-center">
-        { cart.length === 0 ? (
-        <p className="text-xl">Your cart is empty</p> 
-        ) : (
-        cart.map(product => (
-          <div key={product.id} className="flex flex-col items-center">
-            <img src={product.image} alt={product.name} className="w-1/4"/>
-            <h2 className="text-xl font-bold">{product.name}</h2>
-            <p className="text-lg">${product.price}</p>
-            <p className="text-lg">{product.description}</p>
-            <div className="flex gap-x-2">
-              <button onClick={() => handleRemoveFromCart(product.id)}>
-                <i className="fa-solid fa-minus-circle flex items-center"></i>
-              </button>
-              <p className="text-lg">{product.quantity}</p>
-              <button onClick={() => handleAddToCart(product)}>
-                <i className="fa-solid fa-plus-circle flex items-center"></i>
+        <div className="flex mt-10 gap-x-5">
+          {cart.length === 0 ? (
+            <p className="text-xl">Your cart is empty</p>
+          ) : (
+            <div className="flex flex-col">
+              {cart.map(product => (
+                <div key={product.id} className="flex mb-5 px-5 pt-5 pb-5 h-[20vh] w-[70vw] border-2 border-gray-300">
+                  <div className='w-20 h-20 mr-6'>
+                    <img src={product.image} alt={product.name} className="object-fill" />
+                  </div>
+                  <div className="mr-5">
+                    <h2 className="text-xl font-bold">{product.name}</h2>
+                    <p className="text-lg">${product.price}</p>
+                  </div>
+                  <div className="flex gap-x-2">
+                    <button onClick={() => handleRemoveFromCart(product.id)}>
+                      <i className="fa-solid fa-minus-circle flex items-center"></i>
+                    </button>
+                    <p className="text-lg">{product.quantity}</p>
+                    <button onClick={() => handleAddToCart(product)}>
+                      <i className="fa-solid fa-plus-circle flex items-center"></i>
+                    </button>
+                  </div>
+                  <button onClick={() => handleUpdateCart(product)}>
+                    <i className="fa-solid fa-cart-shopping flex items-center"></i>
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+          {cart.length === 0 ? (
+            ""
+          ) : (
+            <div className="border-gray-300 border-2 px-5 h-[10vh]">
+              <p className="text-xl font-bold">Subtotal:</p>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-10 w-full">
+                Checkout
               </button>
             </div>
-            <button onClick={() => handleUpdateCart(product)}>
-              <i className="fa-solid fa-cart-shopping flex items-center"></i>
-            </button>
-          </div>
-        )
-        ))}
+          )}
+        </div>
       </div>
     </div>
   )
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { thunkLoadData } from '../../store/data'
+import { addToCartThunk } from '../../store/cart'
 
 const HomePage = () => {
   const dispatch = useDispatch()
@@ -9,16 +10,36 @@ const HomePage = () => {
   useEffect(() => {
     dispatch(thunkLoadData());
   }, [dispatch]);
+
+  const handleAddToCart = (item) => {
+    const product = {
+      id: item.id,
+      name: item.name,
+      price: item.price,
+      description: item.description,
+      image: item.image_url,
+      quantity: 1
+    }
+    dispatch(addToCartThunk(product));
+  }
   return (
     <div className='mt-10 ml-5'>
       {data && data.products.map((item) => 
-        <div key={item.id} className='flex w-30 h-30 mb-10'>
-          <img className='w-20 h-20' src={item.image_url} alt={item.name} />
-          <div>
-            <h1>{item.name}</h1>
-            <h2>{item.description}</h2>
-            <h3>{item.price}</h3>
+        <div key={item.id} className='flex mb-5 w-[80vw]'>
+          <div className='w-40 h-30 mr-6'>
+            <img className='object-fill' src={item.image_url} alt={item.name} />
           </div>
+          <div>
+            <h1 className='text-xl'>{item.name}</h1>
+            <h3 className='text-xl font-bold'>${item.price}</h3>
+            <h2>{item.description}</h2>
+          </div>
+          <button  
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded h-10'
+            onClick={() => handleAddToCart(item)}
+          >
+            <i className="fa-solid fa-cart-shopping"></i>
+          </button>
         </div>
       )}
     </div>
