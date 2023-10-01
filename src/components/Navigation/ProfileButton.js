a6edfafab85a3361399ca0f50592ec718724c565
nedned1108@github.com <rpmshockwave@gmail.com>
2023-10-01 00:02:48 -0400

    updated nav bar css

--- src/components/Navigation/ProfileButton.js
+++ src/components/Navigation/ProfileButton.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import OpenModalMenuItem from "./OpenModalMenuItem";
 import LoginModal from "../LoginModal";
+import SignupModal from "../SignupModal";
 import { Link } from "react-router-dom";
 import { logout } from "../../store/session";
 
@@ -44,11 +45,11 @@ function ProfileButton({ user }) {
   }
 
   return (
-    <div className="mr-5">
+    <div className="mr-10">
       <button onClick={openMenu} className="menu-button justify-center items-center">
-        <i className="fas fa-solid fa-bars text-lg"></i>
+        <i className="fas fa-solid fa-bars text-xl"></i>
       </button>
-      <div className={`absolute flex flex-col items-start z-10 border-2 border-color-indigo-500 rounded-md p-2 text-md text-color-indigo-500 bg-white mx-0, 
+      <div className={`absolute flex flex-col items-start z-10 top-[31px] right-[1vw] border-2 border-color-indigo-500 rounded-md p-2 text-md text-color-indigo-500 bg-white mx-0, 
         ${showMenu ? '' : 'hidden'}`} ref={ulRef}
       >
         {currentUser ? (
@@ -61,14 +62,19 @@ function ProfileButton({ user }) {
             </li>
           </ul>
         ) : (
-          <ul>
-            <li className="cursor-pointer">
-              <OpenModalMenuItem
-                itemText="Log In"
-                onItemClick={closeMenu}
-                modalComponent={<LoginModal />}
-              />
-            </li>
+          <ul className="">
+            <OpenModalMenuItem
+              className="cursor-pointer"
+              itemText="Log In"
+              onItemClick={closeMenu}
+              modalComponent={<LoginModal />}
+            />
+            <OpenModalMenuItem
+              className="cursor-pointer"
+              itemText="Sign Up"
+              onItemClick={closeMenu}
+              modalComponent={<SignupModal />}
+            />
           </ul>
         )
         }
